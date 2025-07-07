1243e1fd0e6687b962523f2d1e496c98e5cbac5b
penso996 <penso.andrea@outlook.it>
2025-07-07 12:48:15 +0200

    refactor

--- snacks.js
+++ snacks.js
@@ -1,7 +1,7 @@
 // Snack 1
 function getInitials(nomeCompleto) {
-    const [nome, cognome] = nomeCompleto.split(" ");
-    return `${nome.charAt(0)}${cognome.charAt(0)}`;
+    const [nome, cognome] = nomeCompleto.split(" ").filter(str => str !== "");
+    return `${nome.charAt(0).toUpperCase()}${cognome.charAt(0).toUpperCase()}`;
 }
 
 module.exports = {
--- snacks.test.js
+++ snacks.test.js
@@ -3,4 +3,6 @@ const { getInitials } = require("./snacks.js");
 // Snack 1
 test("La funzione getInitials restituisce le iniziali di un nome completo", () => {
     expect(getInitials("Mario Rossi")).toBe("MR");
+    expect(getInitials("andrea penso")).toBe("AP");
+    expect(getInitials("beatrice  penso")).toBe("BP")
 });
